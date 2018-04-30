667dc5a8e6b14ed48bb4e1e6c9c2cc664523cbd3
chillerby <chillerby@turnitin.com>
2018-04-30 13:36:45 +0100

    Default variables set and main.cf/master.cf templates set.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,19 @@
-# postfix_config
+# postfix
 #
 # A description of what this class does
 #This class controls the configuration of postfix and it's related files.
 # @summary A short summary of the purpose of this class
 #
 # @example
-#   include postfix_config
-class postfix_config (
+#   include postfix
+class postfix (
     $package_name = 'postfix',
     $queue_directory = '/var/spool/postfix',
     $config_directory = '/etc/postfix',
     $command_directory = '/usr/sbin',
     $daemon_directory = '/usr/libexec/postfix',
     $data_directory = '/var/lib/postfix',
+    # this is the name of the mailserver, not the hostname of the machine
     $myhostname = 'mx2.turnitin.com',
     $mydomain = undef,
     $myorigin = '$myhostname',
@@ -53,22 +54,29 @@ class postfix_config (
     $service_restart = '/sbin/service postfix reload',
     $dovecot_directory = '/usr/libexec/dovecot',
     $smtpd_recipient_restrictions_list = 'hash:/etc/postfix/bad_recipients',
+    $postscreen = false,
+    $smtp_content_filter = [],
+    $smtps_content_filter = [],
+    $submission = false,
+    $ssl = false,
+    $virtual_transport = false,
+    $master_services = [],
 )
 {
 service { 'postfix':
-    ensure  => running,
     require => Package[$package_name],
+    ensure  => running,
     enable  => true,
 }
 
 file { "${config_directory}/master.cf":
+    require => Package[$package_name],
     content => template('postfix/master.cf.erb'),
     notify  => Service['postfix'],
-    require => Package[$package_name],
 }
 file { "${config_directory}/main.cf":
+    require => Package[$package_name],
     content => template('postfix/main.cf.erb'),
     notify  => Service['postfix'],
-    require => Package[$package_name],
 }
 }
